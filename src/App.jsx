5b08d4d5391f7f6612c59802ca7c8c6903f0807b
SOMIL1 <somilgandhi3@gmail.com>
2025-12-05 11:52:16 +0530

    asdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PAGE from './pages-cont/page1'
+import BLOG from './pages-cont/BLOG';
 
 function App() {
   return (
     <>
-      <PAGE />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PAGE />} />
+          <Route path="/blog" element={<BLOG />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
